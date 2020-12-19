96c9c550da22cd502dab5cdc87b3012456180f00
Sarah Ronald <sareronald@hotmail.com>
2020-12-19 22:07:34 +1100

    fixing issues/bugs with serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -14,62 +14,66 @@ const FILES_TO_CACHE = [
   "/db.js",
   "/index.js",
   "manifest.json",
-  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 //   INSTALL
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
-// The ACTIVATE handler takes care of cleaning up old caches.
+// // The ACTIVATE handler takes care of cleaning up old caches.
 self.addEventListener("activate", (event) => {
-  const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        // return array of cache names that are old to delete
-        return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
-        );
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+  self.clients.claim();
 });
 
-//  FETCH
+//  FETCH **
 self.addEventListener("fetch", (event) => {
   // non GET requests are not cached and requests to other origins are not cached
-  if (event.request.url.startsWith(self.location.origin)) {
+  if (event.request.url.includes("/api/")) {
+    console.log("/api/");
     event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
+      caches.open(DATA_CACHE_NAME).then((cache) => {
+        console.log(cache);
+        return fetch(event.request).then((response) => {
+          if (response.status === 200)
+            cache.put(event.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    event.respondWith(
+      fetch(event.request).catch(function () {
+        return caches.match(event.request).then(function (response) {
+          if (response) {
+            return response;
+          } else if (
+            event.request.headers.get("accept").includes("text/html")
+          ) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
         });
       })
     );
