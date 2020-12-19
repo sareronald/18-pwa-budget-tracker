181e86f0dea15d419d4feaae1ddeeaf442d93ef5
Sarah Ronald <sareronald@hotmail.com>
2020-12-19 12:43:00 +1100

    adding code to SW from Unit 18 classwork

---
+++ public/serviceworker.js
@@ -0,0 +1,74 @@
+// Implement service worker so that users can use the app offline.
+// The SW will need to cache static assets to display the app offline.
+// The SW should cache transaction data, using the cached data as a fallback
+// when the app is used offline. HINT: You should use two caches. One for the
+// static assets such ass html, css, js, images, etc; and another cache for
+// the dynamic data from requests to routes beginning with "/api".
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icons-512x512.png",
+  "/db.js",
+  "/index.js",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
+
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+
+//   INSTALL
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+// The ACTIVATE handler takes care of cleaning up old caches.
+self.addEventListener("activate", (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+//  FETCH
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
