f25bb50db7422e4cf7e403f66fde9af3a0cf9eb4
Sarah Ronald <sareronald@hotmail.com>
2020-12-19 15:27:26 +1100

    fixing server.js connection

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -16,20 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// Connecting to Mongo Atlas (running databse in the cloud)
-// budget database
-const databaseUrl = `mongodb+srv://sareronald:${encodeURIComponent(
-  process.env.MONGO_PWD
-)}@primarycluster0.sxdap.mongodb.net/budget`;
-mongoose.connect(databaseUrl, {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
-
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
 
 // routes
 app.use(require("./routes/api.js"));
